5aac00af7f18292c5f9b0fd473e42be8f2f83cf8
Guillaume Pierson <guillaumep@marmelab.com>
2022-02-28 17:20:49 +0100

    fix lint

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -10,31 +10,61 @@ describe("initial test", () => {
 
 describe("programms arguments", () => {
   it("read argument", () => {
-    main([ "node", "fakejs", "-f=myconfigfile"])
+    main(["node", "fakejs", "-f=myconfigfile"]);
   });
-})
+});
 
-describe('draw gameState',() =>{
-  it("should draw the gamestate", ()=>{
+describe("draw gameState", () => {
+  it("should draw the gamestate", () => {
     let originalLogger = console.log;
-    let output : string[] = []
+    let output: string[] = [];
     console.log = (str: string) => {
       output.push(str);
-    }
+    };
     let gameState = [
       [{ color: 1, symbol: 1 }, null, { color: 2, symbol: 2 }],
       [null, { color: 1, symbol: 3 }, { color: 3, symbol: 3 }],
-      [{ color: 1, symbol: 2 }, null, { color: 3, symbol: 2 }]
-    ]
-    drawGameState(gameState)
+      [{ color: 1, symbol: 2 }, null, { color: 3, symbol: 2 }],
+    ];
+    drawGameState(gameState);
     expect(output.length).toBe(7);
-    expect(output[0]).toBe('┌───┬───┬───┐')
-    expect(output[1]).toBe('│ ' + Colors.Red + '❋' + Colors.Reset + ' │   │ ' + Colors.Green + '♕' + Colors.Reset + ' │')
-    expect(output[2]).toBe('├───┼───┼───┤')
-    expect(output[3]).toBe('│   │ ' + Colors.Red + '◈' + Colors.Reset + ' │ ' + Colors.Yellow + '◈' + Colors.Reset + ' │')
-    expect(output[4]).toBe('├───┼───┼───┤')
-    expect(output[5]).toBe('│ ' + Colors.Red + '♕' + Colors.Reset + ' │   │ ' + Colors.Yellow + '♕' + Colors.Reset + ' │')
-    expect(output[6]).toBe('└───┴───┴───┘')
+    expect(output[0]).toBe("┌───┬───┬───┐");
+    expect(output[1]).toBe(
+      "│ " +
+        Colors.Red +
+        "❋" +
+        Colors.Reset +
+        " │   │ " +
+        Colors.Green +
+        "♕" +
+        Colors.Reset +
+        " │"
+    );
+    expect(output[2]).toBe("├───┼───┼───┤");
+    expect(output[3]).toBe(
+      "│   │ " +
+        Colors.Red +
+        "◈" +
+        Colors.Reset +
+        " │ " +
+        Colors.Yellow +
+        "◈" +
+        Colors.Reset +
+        " │"
+    );
+    expect(output[4]).toBe("├───┼───┼───┤");
+    expect(output[5]).toBe(
+      "│ " +
+        Colors.Red +
+        "♕" +
+        Colors.Reset +
+        " │   │ " +
+        Colors.Yellow +
+        "♕" +
+        Colors.Reset +
+        " │"
+    );
+    expect(output[6]).toBe("└───┴───┴───┘");
     console.log = originalLogger;
   });
-})
+});
--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,46 @@
 import { GameState } from "./GameState";
 import { Colors } from "./Colors";
-import fs from 'fs';
+import fs from "fs";
 
-const symbols = {1:"❋", 2:"♕", 3:"◈"}
+const symbols = { 1: "❋", 2: "♕", 3: "◈" };
 
-const colors = {1:Colors.Red, 2:Colors.Green, 3:Colors.Yellow}
-const topLine =  "┌───┬───┬───┐"
-const rowLine =  "├───┼───┼───┤"
-const baseLine = "└───┴───┴───┘"
+const colors = { 1: Colors.Red, 2: Colors.Green, 3: Colors.Yellow };
+const topLine = "┌───┬───┬───┐";
+const rowLine = "├───┼───┼───┤";
+const baseLine = "└───┴───┴───┘";
 
-export function drawGameState(gameState : GameState){
-    console.log(topLine);
-    for(let i=0;i<gameState.length; i++){
-        let rowCharacters = "│";
-        for(let j = 0; j<gameState[i].length; j++){
-            let cell = gameState[i][j];
-            rowCharacters += ' ' + (cell ?  colors[cell.color] + symbols[cell.symbol] + Colors.Reset  + ' ' : '  ') + '│';
-        }
-        console.log(rowCharacters);
-        if( i !== gameState.length-1){
-        console.log(rowLine);
+export function drawGameState(gameState: GameState) {
+  console.log(topLine);
+  for (let i = 0; i < gameState.length; i++) {
+    let rowCharacters = "│";
+    for (let j = 0; j < gameState[i].length; j++) {
+      let cell = gameState[i][j];
+      rowCharacters +=
+        " " +
+        (cell
+          ? colors[cell.color] + symbols[cell.symbol] + Colors.Reset + " "
+          : "  ") +
+        "│";
     }
-
+    console.log(rowCharacters);
+    if (i !== gameState.length - 1) {
+      console.log(rowLine);
     }
-    console.log(baseLine);
+  }
+  console.log(baseLine);
 }
 
-export function main(args : string[]) {
+export function main(args: string[]) {
+  const myArgs = args.slice(2);
 
-    const myArgs = args.slice(2);
+  const myConfigFileName = myArgs[0].split("=")[1];
 
-    const myConfigFileName = myArgs[0].split("=")[1];
+  const data = fs.readFileSync(myConfigFileName, "utf8");
 
-   
-
-    const data = fs.readFileSync( myConfigFileName, 'utf8');
-
-    let gameState = JSON.parse(data) as GameState;
-    drawGameState(gameState );
-   
+  let gameState = JSON.parse(data) as GameState;
+  drawGameState(gameState);
 }
 
 if (process.env.JEST_WORKER_ID === undefined) {
-    main(process.argv);
+  main(process.argv);
 }
