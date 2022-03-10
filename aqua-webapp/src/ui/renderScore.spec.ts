bd41c543b3f31174f7f7c6b3c27143c6f40461ab
Cmanel <cindy@marmelab.com>
2022-03-10 13:49:01 +0100

    add test e2e

--- aqua-webapp/src/ui/renderScore.spec.ts
+++ aqua-webapp/src/ui/renderScore.spec.ts
@@ -1,47 +1,38 @@
 import { Score } from "@aqua/core";
+import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import { renderScore } from "./renderScore";
 
 describe("renderScore", () => {
-  it("it should say that Symbol win when Symbol score > color score", () => {
+  it("should say that Symbol win when Symbol score > color score", () => {
     const score: Score = {
       color: 12,
       symbol: 15,
     };
-    const htmlCode = renderScore(score);
-    const wantedResult =
-      `<div>Colors : 12</div>` +
-      `<div> Symbols: 15</div>` +
-      `<div>And the winner is :</div>` +
-      `<div class="winner">` +
-      `Symbol` +
-      `</div>`;
-    expect(htmlCode).toBe(wantedResult);
+
+    expect(renderScore(score)).toContain(
+      '<div>And the winner is :</div><div class="winner">Symbol</div>',
+    );
   });
 
-  it("it should say that Color win when Color score > Symbol score", () => {
+  it("should say that Color win when Color score > Symbol score", () => {
     const score: Score = {
       color: 15,
       symbol: 12,
     };
     const htmlCode = renderScore(score);
-    const wantedResult =
-      `<div>Colors : 15</div>` +
-      `<div> Symbols: 12</div>` +
-      `<div>And the winner is :</div>` +
-      `<div class="winner">` +
-      `Color` +
-      `</div>`;
-    expect(htmlCode).toBe(wantedResult);
+    expect(htmlCode).toContain(
+      '<div>And the winner is :</div><div class="winner">Color</div>',
+    );
   });
-  it("it should say draw when Color score = Symbol score", () => {
+  it("should say draw when Color score = Symbol score", () => {
     const score: Score = {
       color: 15,
       symbol: 15,
     };
     const htmlCode = renderScore(score);
-    const wantedResult =
-      `<div>Colors : 15</div>` + `<div> Symbols: 15</div>` + `<div>draw</div>`;
-    expect(htmlCode).toBe(wantedResult);
+
+    expect(htmlCode).toContain("<div>draw</div>");
   });
 });
