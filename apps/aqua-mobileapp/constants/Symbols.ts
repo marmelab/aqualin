ea07291af57bd9d419beb7cc392a6be31d959d9c
Guillaume Pierson <guillaumep@marmelab.com>
2022-03-15 17:44:52 +0100

    Render token in mobile app

---
+++ apps/aqua-mobileapp/constants/Symbols.ts
@@ -0,0 +1,11 @@
+import { ImageSourcePropType } from "react-native";
+
+export const Symbols: { [key: number]: ImageSourcePropType } = {
+  100: require("../assets/images/dot.png"),
+  0: require("../assets/images/octopus.png"),
+  1: require("../assets/images/seahorse.png"),
+  2: require("../assets/images/crab.png"),
+  3: require("../assets/images/starfish.png"),
+  4: require("../assets/images/fish.png"),
+  5: require("../assets/images/turtle.png"),
+};
--- apps/aqua-mobileapp/screens/GameScreen.tsx
+++ apps/aqua-mobileapp/screens/GameScreen.tsx
@@ -13,13 +13,13 @@ export default function GameScreen({gameTemplate}: GameProps) {
   gameTemplate = {
     gameState: {
       board: [[null, {symbol: 2, color: 4}, null, null, null, null],
-      [null, {symbol: 2, color: 4}, null, null, null, null],
+      [null, {symbol: 2, color: 0}, null, null, null, null],
       [null, null, null, null, {symbol: 4, color: 5}, null],
-      [null, {symbol: 2, color: 4}, null, null, null, null],
-      [null, null, {symbol: 2, color: 4}, {symbol: 2, color: 6}, null, null],
-      [null, {symbol: 2, color: 4}, null, null, null, null],],
-      river: [{symbol: 1, color: 2}, {symbol: 1, color: 4}, {symbol: 1, color: 5},
-         {symbol: 2, color: 2}, {symbol: 5, color: 1}, {symbol: 6, color: 2}],
+      [null, {symbol: 2, color: 1}, null, null, null, null],
+      [null, null, {symbol: 3, color: 4}, {symbol: 2, color: 3}, null, null],
+      [null, {symbol: 2, color: 5}, null, null, null, null],],
+      river: [{symbol: 0, color: 0}, {symbol: 1, color: 1}, {symbol: 2, color: 2},
+         {symbol: 3, color: 3}, {symbol: 4, color: 4}, {symbol: 5, color: 5}],
       playerTurn: "Color"
     }
   } as GameTemplate;
