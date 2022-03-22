4acd2a1c783ba146d909a7bee7f121c18b35b278
Cimanel <cindy@marmelab.com>
2022-03-22 10:09:19 +0100

    tsx files and remove useless files

--- apps/aqua-admin/src/Dashboard.js
+++
@@ -1,9 +0,0 @@
-import { Card, CardContent, CardHeader } from '@mui/material';
-import * as React from "react";
-
-export default () => (
-    <Card>
-        <CardHeader title="Welcome to the administration" />
-        <CardContent>Lorem ipsum sic dolor amet...</CardContent>
-    </Card>
-);
---
+++ apps/aqua-admin/src/Dashboard.tsx
@@ -0,0 +1,11 @@
+import { Card, CardContent, CardHeader } from '@mui/material';
+import * as React from "react";
+
+
+const Dashboard = ()=>{
+    <Card>
+    <CardHeader title="Welcome to the administration" />
+    <CardContent>Lorem ipsum sic dolor amet...</CardContent>
+</Card>
+}
+export default Dashboard;
