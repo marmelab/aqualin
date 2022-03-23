165c013998ed8899881d84f7181e01a3bb275ba3
Guillaume Pierson <guillaumep@marmelab.com>
2022-03-23 19:23:27 +0100

    fix account name

--- apps/aqua-webapp/src/user/migrations/1648059043604-AddRoleToAdmin.ts
+++ apps/aqua-webapp/src/user/migrations/1648059043604-AddRoleToAdmin.ts
@@ -5,7 +5,7 @@ import { User } from "../entities/user.entity";
 export class AddRoleToAdmin1648059043604 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     const admins = await queryRunner.manager.find<User>("user", {
-      username: "aaa",
+      username: "admin",
     });
     if (admins.length === 0) {
       throw new Error("Admin account not found");
