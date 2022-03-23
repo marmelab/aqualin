19ddcf771cea1e6efc3efc850032545e25d56925
Guillaume Pierson <guillaumep@marmelab.com>
2022-03-23 19:17:28 +0100

    add admin accoun

---
+++ apps/aqua-webapp/src/user/migrations/1648059043604-AddRoleToAdmin.ts
@@ -0,0 +1,19 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+import { User } from "../entities/user.entity";
+
+export class AddRoleToAdmin1648059043604 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const admins = await queryRunner.manager.find<User>("user", {
+      username: "aaa",
+    });
+    if (admins.length === 0) {
+      throw new Error("Admin account not found");
+    }
+    const user = admins[0];
+    user.admin = true;
+    queryRunner.manager.save(user);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
